716ee2e69cfb51824273d3b3e4ad2b05eb68baf5
luisss29 <luisshu29@gmail.com>
2021-05-31 23:15:08 +0700

    menambahkan button logout

--- application/views/account/dashboard.php
+++ application/views/account/dashboard.php
@@ -4,6 +4,9 @@
 	<title>Login Berhasil</title>
 </head>
 <body>
-<p>Selamat datang</p>
+	<p>Selamat datang</p>
+	<button class="btn btn-danger">
+		<a href="<?php echo base_url('Account/logout'); ?>">Logout</a>
+	</button>
 </body>
 </html>
