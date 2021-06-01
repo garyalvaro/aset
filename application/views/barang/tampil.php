e34e0b7814c912cd5de8e9bb8770b40089b11f83
garyalvaro <garyalvarowijaya@gmail.com>
2021-06-01 16:15:02 +0700

    Tambahkan modal di tampil.php untuk edit stok

--- application/views/barang/tampil.php
+++ application/views/barang/tampil.php
@@ -1,9 +1,10 @@
 <?php $this->load->view('layout/headerA'); ?>
-    <!-- CSS PLUGINS START  -->
+<!-- CSS PLUGINS START  -->
 
-    
+	<!-- Bootstrap Touchspin css -->
+	<link href="<?=base_url();?>assets/plugins/bootstrap-touchspin/css/jquery.bootstrap-touchspin.min.css" rel="stylesheet" />
 
-    <!-- CSS PLUGINS END  -->
+<!-- CSS PLUGINS END  -->
 <?php $this->load->view('layout/headerB'); ?>
 
 <?php $this->load->view('layout/navbar'); ?>
@@ -12,7 +13,6 @@
 
 <div class="wrapper">
             <div class="container-fluid">
-                
                 <div class="row">
                     <div class="col-lg-3">
                         <div class="card m-b-30 card-body">
@@ -32,7 +32,6 @@
                 <div class="row"> 
                     <?php if( ! empty($barang)): ?>
                         <?php foreach($barang as $data): ?>  
-
                             <div class="col-6 col-md-6 col-lg-6 col-xl-3 divBesar">
                                 <!-- Simple card -->
                                 <div class="card m-b-30">
@@ -41,21 +40,72 @@
                                         <h4 class="card-title font-16 mt-0 judul"><?= $data->nama_barang; ?></h4>
                                         <p class="card-text"><?= $data->deskripsi; ?></p>
                                         <a href="<?=base_url("barang/editBarang/".$data->id_barang);?>" class="btn btn-outline-secondary waves-effect waves-light m-b-10">Edit Data</a>
-                                        <a href="<?=base_url("barang/editStok/".$data->id_barang);?>" class="btn btn-primary waves-effect waves-light">Update Stok</a>
+                                        <button type="button" class="btn btn-primary waves-effect waves-light m-b-10" data-toggle="modal" data-target=".bs-example-modal-sm<?= $data->id_barang; ?>">Update Stok</button>
                                     </div>
                                 </div>
                             </div>
-
+                            
                         <?php endforeach; ?>
                     <?php endif; ?>    
                 </div>
-
-                    
                 <!-- end row -->
             </div> <!-- end container-fluid -->
         </div>
         <!-- end wrapper -->
 
+        
+        <?php if( ! empty($barang)): ?>
+        <?php foreach($barang as $data): ?> 
+        <div class="modal fade bs-example-modal-sm<?= $data->id_barang; ?>" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-sm">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title mt-0" id="mySmallModalLabel">Edit Stok <?= $data->nama_barang; ?></h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    </div>
+                    <div class="modal-body">
+                        <?php echo form_open_multipart('barang/editStok/'.$data->id_barang); ?>
+                            <div class="form-group row">
+                                <label for="alasan" class="col-sm-12 col-form-label">Alasan Ubah Stok</label>
+                                <div class="col-sm-12">
+                                    <select class="form-control" id="action" name="action" required>
+                                    <option value="" disabled>Pilih Alasan</option>
+                                    <option value="0">Rusak</option>
+                                    <option value="4">Hapus</option>
+                                    <option value="3">Tambah</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="deskripsi" class="col-sm-12 col-form-label">Deskripsi</label>
+                                <div class="col-sm-12">
+                                    <textarea class="form-control" id="deskripsi" name="deskripsi" autocomplete="off"></textarea>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="deskripsi" class="col-sm-12 col-form-label">Jumlah</label>
+                                <div class="col-sm-12">
+                                    <input id="qty" type="text" name="qty" value="1">
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <div class="col-sm-12 text-right m-t-20">
+                                    <button class="btn btn-outline-secondary" data-dismiss="modal" aria-hidden="true">Batal</button>
+                                    &nbsp; &nbsp;
+                                    <input class="btn btn-primary" type="submit" name="submit" value="Simpan">
+                                </div>
+                            </div>
+
+                        <?php echo form_close(); ?></div>
+                    </div>
+                </div><!-- /.modal-content -->
+            </div><!-- /.modal-dialog -->
+        </div><!-- /.modal -->
+        <?php endforeach; ?>
+        <?php endif; ?> 
 
 <?php $this->load->view('layout/footerA'); ?>
 <!-- JS PLUGINS START  -->
@@ -82,6 +132,37 @@
         }
     }
 </script>
+
+
+<!-- Bootstrap Touchpin js -->
+<script src="<?=base_url()?>assets/plugins/bootstrap-touchspin/js/jquery.bootstrap-touchspin.min.js"></script>
+<script type="text/javascript">
+!function($) {
+    "use strict";
+
+    var AdvancedForm = function() {};
+    
+    AdvancedForm.prototype.init = function() {
+
+		$("input[name='qty']").TouchSpin({
+			min: 1,
+			step: 1,
+			buttondown_class: 'btn btn-secondary',
+			buttonup_class: 'btn btn-secondary'
+		});
+	},
+    //init
+    $.AdvancedForm = new AdvancedForm, $.AdvancedForm.Constructor = AdvancedForm
+}(window.jQuery),
+
+//initializing
+function ($) {
+    "use strict";
+    $.AdvancedForm.init();
+}(window.jQuery);
+
+</script>
+
 
 <!-- JS PLUGINS END  -->
 <?php $this->load->view('layout/footerA'); ?>
