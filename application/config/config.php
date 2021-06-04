e37a4d574aded183e116fc62fbd44c0108bfb1db
garyalvaro <garyalvarowijaya@gmail.com>
2021-06-04 21:11:30 +0700

    Tambahkan php datetime Asia/Jakarta

--- application/config/config.php
+++ application/config/config.php
@@ -521,3 +521,8 @@ $config['rewrite_short_tags'] = FALSE;
 | Array:		array('10.0.1.200', '192.168.5.0/24')
 */
 $config['proxy_ips'] = '';
+
+
+// Datetime Jakarta
+date_default_timezone_set('Asia/Jakarta');
+       
